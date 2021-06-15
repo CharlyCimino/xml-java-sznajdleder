74a76288e4a3141449f3f3adc4669af8b0e86d90
Charly Cimino <carlosecimino@gmail.com>
2021-06-15 17:11:53 -0300

    Proyecto creado

---
+++ src/xml/Principal.java
@@ -0,0 +1,15 @@
+package xml;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
